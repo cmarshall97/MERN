e6fdc73de6d7d27c1f1bf37a6e86ba7994ea0234
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-06 17:35:20 -0400

    last commit for practice react app

---
+++ React/assignments/my-first-project/src/components/Component.js
@@ -0,0 +1,14 @@
+const Component = () => {
+    //JSX
+    //transpiled into ES5 Javascript using Babel
+
+    return(
+        <div>
+            <h1 className="my-class">Hello World!</h1>
+        </div>
+    )
+}
+
+
+export default Component;
+//allows this to be found andbused in other components
