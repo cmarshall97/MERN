d3cb4b8de2abc763b7e3f7d923884a22db524ee6
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-09 14:14:36 -0400

    adding useState to increase age

--- React/assignments/prop-it-up/src/App.js
+++ React/assignments/prop-it-up/src/App.js
@@ -6,25 +6,25 @@ function App() {
   return (
     <div className="App">
       <PersonCard 
-      lastName={"Marshall"} 
-      firstName={"Carol"}
-      age={"25"}
-      hairColor={"Blonde"}/>
+        lastName={"Marshall"} 
+        firstName={"Carol"}
+        age={25}
+        hairColor={"Blonde"}/>
       <PersonCard 
-      lastName={"Rineer"} 
-      firstName={"Grace"}
-      age={"26"}
-      hairColor={"Black"}/>
+        lastName={"Rineer"} 
+        firstName={"Grace"}
+        age={26}
+        hairColor={"Black"}/>
       <PersonCard 
-      lastName={"McMullen"} 
-      firstName={"Ryne"}
-      age={"29"}
-      hairColor={"Strawberry Blonde"}/>
+        lastName={"McMullen"} 
+        firstName={"Ryne"}
+        age={29}
+        hairColor={"Strawberry Blonde"}/>
       <PersonCard 
-      lastName={"Hunt"} 
-      firstName={"Sam"}
-      age={"33"}
-      hairColor={"Brown"}/>
+        lastName={"Hunt"} 
+        firstName={"Sam"}
+        age={33}
+        hairColor={"Brown"}/>
     </div>
   );
 }
--- React/assignments/prop-it-up/src/components/PersonCard.js
+++ React/assignments/prop-it-up/src/components/PersonCard.js
@@ -1,10 +1,13 @@
-const PersonCard = (props) => {
+import { useState } from 'react';
 
+const PersonCard = props => {
+    const [getAge, setAge] = useState(props.age); 
     return (
         <div>
             <h2>{ props.lastName } , { props.firstName }</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {getAge}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={(event)=> setAge(getAge + 1)}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
