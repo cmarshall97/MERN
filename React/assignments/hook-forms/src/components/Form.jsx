8ae996baa7951707b5eb974ad143fc797ca192d2
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-12 15:20:24 -0400

    Adding validations to the form (more forms)

--- React/assignments/hook-forms/src/components/Form.jsx
+++ React/assignments/hook-forms/src/components/Form.jsx
@@ -9,32 +9,55 @@ const Form = (props) => {
     const [confirmPassword, setConfirmPassword] = useState('')
 
 //used for testing info in console and making sure we remain in state when we refresh
-// const userInfo = { 
-//     firstName,
-//     lastName,
-//     email,
-//     password,
-//     confirmPassword
-// }
+const userInfo = { 
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword
+}
 
-// const submitUser = (e) => {
-//     e.preventDefault()
-//     console.log(userInfo)
-// }
+const submitUser = (e) => {
+    e.preventDefault() //keeps info from refreshing and losing state
+    if (firstName && lastName && email && password && confirmPassword){ //if there is information in each than...
+        console.log(userInfo) //console log it
+    } else { //if not then 
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setPassword('')
+        setConfirmPassword('')
+    }
+}
 
     return (
         <div>
             <form onSubmit={submitUser}>
-                <label>First name:</label>
+                <label>First name: </label>
                 <input type="text" name="firstName" onChange = {(e)=> setFirstName(e.target.value)}/>
-                <label>Last name:</label>
+                { //if the firstname has data and the length is less than 2 then render this message as validation error
+                    firstName && firstName.length < 2 ? <p>First name must be more than 2 characters!</p> : null
+                }
+                <label>Last name: </label>
                 <input type="text" name="lastName" onChange = {(e)=> setLastName(e.target.value)}/>
-                <label>email:</label>
+                {
+                    lastName && lastName.length < 2 ? <p>Last name must be more than 2 characters!</p> : null
+                }
+                <label>Email: </label>
                 <input type="text" name="email" onChange = {(e)=> setEmail(e.target.value)}/>
-                <label>Password:</label>
-                <input type="text" name="password" onChange = {(e)=> setPassword(e.target.value)}/>
-                <label>Confirm Password:</label>
-                <input type="text" name="confirmPassword" onChange = {(e)=> setConfirmPassword(e.target.value)}/>
+                {
+                    email && email.length < 5 ? <p>Email must be more than 5 characters!</p> : null
+                }
+                <label>Password: </label>
+                <input type="password" name="password" onChange = {(e)=> setPassword(e.target.value)}/>
+                {
+                    password && password.length < 8 ? <p>Password must be at least 8 characters!</p> : null
+                }
+                <label>Confirm Password: </label>
+                <input type="password" name="confirmPassword" onChange = {(e)=> setConfirmPassword(e.target.value)}/>
+                {
+                    confirmPassword && confirmPassword.length < 8 ? <p>Password must be at least 8 characters!</p> : null
+                }
                 <button>Submit</button>
             </form>
             <p>Your Form Data</p>
