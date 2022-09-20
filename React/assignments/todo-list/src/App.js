9deccc716671282fbdf4184b32d17f7d9b3412e8
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-19 22:03:48 -0400

    adding to list and not allowing page to refresh

--- React/assignments/todo-list/src/App.js
+++ React/assignments/todo-list/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [itemInfo,setItemInfo] = useState('')
+  const[list, setList] = useState([])
+
+  const submitToDoItem = (e) => {
+    e.preventDefault()
+    setList([...list,itemInfo]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form onSubmit={e => submitToDoItem(e)} >
+        <input type="text" onChange={e=>setItemInfo(e.target.value)} name="itemInfo" value={itemInfo}></input>
+        <button> Add </button>
+      </form>
     </div>
   );
 }
