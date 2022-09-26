e3b9d28c3ff52865c286d141332fbd0ca13a93fd
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-26 13:46:51 -0400

    dependency array

--- React/assignments/pokemon-api/src/App.js
+++ React/assignments/pokemon-api/src/App.js
@@ -1,7 +1,18 @@
+import {useState,useEffect} from 'react';
+//useEffect handles side effects
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState(0)
+
+  useEffect(() =>{
+    setState(state +1)
+  },[])
+  //dependency array is an empy array where there is no conditiion in which it should run again 
+
   return (
     <div className="App">
       
