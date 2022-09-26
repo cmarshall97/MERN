d1b231f7805e66d3e51a224d7b09b3eeb3a977d7
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-26 13:17:42 -0400

    commit

--- React/assignments/todo-list/src/App.js
+++ React/assignments/todo-list/src/App.js
@@ -9,10 +9,21 @@ function App() {
   const submitToDoItem = (e) => {
     e.preventDefault()
     setList([...list,newItem])
-  }
+    //allows empty items to not be added
+    if (newItem.length == 0){
+      return;
+    }
 
+    const todoItem = {
+      text: newItem,
+      complete: false
+    };
+  }
+//delete function
   const deleteToDoItem = (i) => {
+    //do a loop over the array your working with and give you the index of the item
     const filteredList = list.filter((newItem, index) => {
+      // if the index does not equal the index (i) of the item I want to delete, I want to keep it. If the index matches then it return true and we delete the item
       return index!== i;
     });
 
@@ -29,7 +40,8 @@ function App() {
         {
           list.map((newItem, index) => {
             return (
-              <ul>
+              <ul style={{display: 'flex'}}>
+                <input type="checkbox" ></input>
                 <li key={index} input='checkbox'> {newItem} </li>
                 <button onClick={e=>deleteToDoItem(index)}> Delete </button>
               </ul>
