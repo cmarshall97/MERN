afa191d57153224b0122c27112d45218d6d62684
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-26 15:34:22 -0400

    grabbing 807 pokemon immediatly when page renders

--- React/assignments/pokemon-api/src/App.js
+++ React/assignments/pokemon-api/src/App.js
@@ -1,21 +1,38 @@
 import {useState,useEffect} from 'react';
 //useEffect handles side effects
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [state, setState] = useState(0)
+  const [state, setState] = useState([])
 
   useEffect(() =>{
-    setState(state +1)
+    //base URL from API which is a request to an API endpoint/address
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+    //two functions chained together on fetch function
+    //.then handles success
+    //.catch handles any errors
+    .then((result)=>{
+      return result.json() //convert what we get back to json
+    }).then((res)=>{
+      console.log(res)
+      setState(res.results)
+    }).catch((error)=>{
+      console.log(error)
+    })
   },[])
   //dependency array is an empy array where there is no conditiion in which it should run again 
 
   return (
     <div className="App">
-      
+      {
+        state.map((pokemon, index)=>(
+          <div>
+            <h1 key={index}> {pokemon.name} </h1>
+          </div>
+        ))
+      }
     </div>
   );
 }
