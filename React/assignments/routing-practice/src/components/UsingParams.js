ca6deecc1b0243abed1f32add73cb5d89d27be47
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-09-30 14:15:07 -0400

    adding params using styling w/ colors

--- React/assignments/routing-practice/src/components/UsingParams.js
+++ React/assignments/routing-practice/src/components/UsingParams.js
@@ -2,12 +2,16 @@ import {useParams} from 'react-router-dom';
 
 const UsingParams = (props) => {
 
-    const {word,color,backColor} = useParams()
+    const {word,color,backColor} = useParams();
     return(
         <div>
             {
                 isNaN(word) ? 
-                <h2 style={color} >The word is : {word} </h2>
+                <h2 style= {
+                    color? //does this exist?
+                    {color: color, backgroundColor:backColor} //if it does than apply these properties
+                    :null //if it does not exist than we get null
+                } >The word is : {word} </h2>
                 :
                 <h2>The number is : {word} </h2>
             }
