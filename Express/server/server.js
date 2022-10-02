ad30d91d496c9e77825f9ae115a15a9cc31b39f2
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-10-02 11:36:16 -0400

    creating a server and installing faker api

--- Express/server/server.js
+++ Express/server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
+// Express Middleware functions
+// make sure these lines are above any app.get or app.post code blocks
+app.use(express.json()); //allows us to parse JON objects coming in from POSTS and PUT requests
+app.use(express.urlencoded({extended: true})); //allows us to parse strings and arrays comig in with JSON object
+
+app.get("/api/welcome", (request,response)=>{
+    console.log("this is the welcome API route");
+    response.json({message: "Welcome to our API... THIS IS THE RESPONSE"})
+})
+
+app.post("/api/welcome", (request,response)=>{
+    console.log("this is our post request");
+    response.json({
+        message: "MERN is great....this is our post response ",
+        ourRequestBody: request.body
+    })
+    console.log(request.body)
+})
 
 
 
 
-
-
-
-
-
-app.listen(8000,()=>console.log("You have successfully connected to port 8000"))
+app.listen(8000,()=>console.log("Successfully connected to port 8000"))
