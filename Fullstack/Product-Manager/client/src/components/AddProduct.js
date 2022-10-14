91455a72d97ecd94b6a56851c113e920230d2010
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-10-14 12:46:17 -0400

    addes form in components to add product

---
+++ Fullstack/Product-Manager/client/src/components/AddProduct.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const ProductForm = () => {
+    //keep track of what is being typed via useState hook
+    const [title, setTitle] = useState(""); 
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+    //handler when the form is submitted
+    const onSubmitHandler = (e) => {
+        //prevent default behavior of the submit
+        e.preventDefault();
+        //make a post request to create a new person
+        axios.post('http://localhost:8000/api/people', {
+            title,
+            price,
+            description      
+        })
+            .then(res=>{
+                console.log(res); // always console log to get used to tracking your data!
+                console.log(res.data);
+            })
+            .catch(err=>console.log(err))
+    }
+    
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <h1>Product Manager</h1>
+            <div>
+                <label>Title</label><br/>
+                    {/* When the user types in this input, our onChange synthetic event 
+                    runs this arrow function, setting that event's target's (input) 
+                    value (what's typed into the input) to our updated state   */}
+                <input type="text" onChange = {(e)=>setTitle(e.target.value)}/>
+            </div>
+            <div>
+                <label>Price</label><br/>
+                <input type="text" onChange = {(e)=>setPrice(e.target.value)}/>
+            </div>
+            <div>
+                <label>Description</label><br/>
+                <input type="text" onChange = {(e)=>setDescription(e.target.value)}/>
+            </div>
+            <input type="submit"/>
+        </form>
+    )
+}
+export default ProductForm;
